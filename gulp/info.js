f0afdaf4771b8e9422ddc1bb0bc12663c5a8046c
jupl <juan.placencia.512@gmail.com>
2014-10-11 16:58:31 -0500

    Add affected files in info

--- gulp/info.js
+++ gulp/info.js
@@ -1,11 +1,16 @@
 'use strict';
 
+var globule = require('globule');
 var gulp = require('gulp');
 var path = require('path');
 var rump = require('rump');
 var util = require('gulp-util');
 
 gulp.task('rump:info:static', function() {
+  var glob = path.join(rump.configs.main.paths.source.root,
+                       rump.configs.main.paths.source.static,
+                       rump.configs.main.globs.build.static);
+  var files = globule.find([glob].concat(rump.configs.main.globs.global));
   var source = path.join(rump.configs.main.paths.source.root,
                          rump.configs.main.paths.source.static);
   var destination = path.join(rump.configs.main.paths.destination.root,
@@ -13,6 +18,13 @@ gulp.task('rump:info:static', function() {
 
   util.log('Static files are copied from', util.colors.green(source),
            'to', util.colors.green(destination));
+
+  if(files.length) {
+    util.log('Affected files:');
+    files.forEach(function(file) {
+      util.log(util.colors.blue(path.relative(source, file)));
+    });
+  }
 });
 
 gulp.tasks['rump:info'].dep.push('rump:info:static');

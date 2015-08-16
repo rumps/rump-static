aa8304bb52cd76b7670c0a37fc658933097530a0
jupl <juan.placencia.512@gmail.com>
2015-08-16 01:08:42 -0500

    Set timeout to 0 at top level

--- test/gulp.js
+++ test/gulp.js
@@ -10,7 +10,9 @@ import {spy} from 'sinon'
 
 const {stripColor} = colors
 
-describe('tasks', () => {
+describe('tasks', function() {
+  this.timeout(0)
+
   beforeEach(() => {
     rump.configure({paths: {
       source: {root: 'test/src', static: ''},
@@ -50,7 +52,7 @@ describe('tasks', () => {
 
     before(async(done) => {
       original = await readFile('test/src/index.html')
-      gulp.task('postbuild', ['spec:watch'], () => timeout(800).then(done))
+      gulp.task('postbuild', ['spec:watch'], () => done())
       gulp.start('postbuild')
     })
 
@@ -59,9 +61,9 @@ describe('tasks', () => {
     it('handles updates', async() => {
       let content = await readFile('tmp/index.html')
       bufferEqual(content, original).should.be.true()
-      await timeout(800)
+      await timeout(1000)
       await writeFile('test/src/index.html', '<h1>New</h1>')
-      await timeout(800)
+      await timeout(1000)
       content = await readFile('tmp/index.html')
       bufferEqual(content, original).should.be.false()
     })
